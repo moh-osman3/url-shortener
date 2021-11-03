cbc319d518c56987892ced83559b71e95d09f816
Moh Osman <mosman@Mohs-MacBook-Air.local>
2021-11-03 03:11:23 -0700

    shortening url, still need to figure out the redirect

--- urlapp.py
+++ urlapp.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template, request
+import hashlib
 
 app = Flask(__name__)
 
+url2short = {}
 @app.route("/")
 
 def home():
+    dct = {}
     req = request.args.get('fname')
     if req == None:
         req = ""
-    print(req)
-    return render_template("home.html", out=req)
+    id_endpt = hashlib.sha256(req.encode('utf-8')).hexdigest()[:4]
+
+    print(id_endpt)
+    if id_endpt not in url2short:
+        url2short[id_endpt] = req
+
+    return render_template("home.html", out="localhost/{}".format(id_endpt))
+
 
 
 if __name__ == "__main__":
